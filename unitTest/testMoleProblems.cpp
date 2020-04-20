7d2805735c326e054fa29d421279a7538b11a764
Zack Taylor <rtaylo45@vols.utk.edu>
2020-04-19 23:19:59 -0400

    WIP: Added the solutions for 3 and 4. Changed the initial conditions a little bit. Added problem 5.

--- unitTest/testMoleProblems.cpp
+++ unitTest/testMoleProblems.cpp
@@ -1,3 +1,4 @@
+#define _USE_MATH_DEFINES
 #include <string>
 #include <math.h>
 #include <cmath>
@@ -52,7 +53,7 @@ void moleProblem1(int myid){
 	std::ofstream outputFile;
 	outputFileName = "moleProblem1.out";
 	outputFile.open(outputFileName, std::ios::out | std::ios::trunc);
-	outputFile << "dx"	<< " " << "dt" << " " << "Max l2 Error" << "\n";
+	outputFile << "Total problem time: " << tEnd << "\n";
 
 	// Loops over number of cells
 	for (int &xCells : numOfxCells){
@@ -65,6 +66,9 @@ void moleProblem1(int myid){
 		// Loops over number of time steps
 		for (double &numOfSteps	: steps){
 			dt = tEnd/numOfSteps;
+			outputFile << "dx: " << xLength/(double)xCells << "\n";
+			outputFile << "dt: " << dt << "\n";
+			outputFile << "x"	<< " " << "C" << "\n";
 
 			// Add specs
 			cID = spec.addSpecies(1.0, 0.0, 0.0);
@@ -95,27 +99,29 @@ void moleProblem1(int myid){
 				t = step*dt;
 				// Solve with CRAM
 				spec.solve(t);
+			}
 
-				// Gets species Concentrations
-				if (myid==0){
-					for (int i = 0; i < xCells; i++){
-						for (int j = 0; j < yCells; j++){
-							cell = model.getCellByLoc(i,j);	
+			// Gets species Concentrations
+			if (myid==0){
+				for (int i = 0; i < xCells; i++){
+					for (int j = 0; j < yCells; j++){
+						cell = model.getCellByLoc(i,j);	
 
-							// Caclulate analytical solution
-							xc = cell->x;
-							cSol = xc*exp(-lambda*t);
+						// Caclulate analytical solution
+						xc = cell->x;
+						cSol = xc*exp(-lambda*t);
 
-							// Get libowski solution
-							cCon = spec.getSpecies(i, j, cID);
+						// Get libowski solution
+						cCon = spec.getSpecies(i, j, cID);
 
-							//assert(isApprox(VSol, VCon, 1e-5, 1e-4));
-							linfError = std::max(linfError, std::abs(cSol-cCon)/cSol);
-						}
+						//assert(isApprox(VSol, VCon, 1e-5, 1e-4));
+						linfError = std::max(linfError, std::abs(cSol-cCon)/cSol);
+						outputFile << xc << " " << cCon << "\n";
 					}
 				}
-				outputFile << dx << " " << dt << " " << linfError << "\n";
 			}
+			outputFile << "\n";
+			//std::cout << dx << " " << dt << " " << linfError << "\n";
 			// Clean species
 			spec.clean();
 		}
@@ -137,23 +143,23 @@ void moleProblem1(int myid){
 //			x = [0, 100]
 //			t = [0, 20]
 //			v = 2.0
-//			lambda = 1.0
+//			lambda = 0.01
 //
 //	Initial conditions and BC's:
-//			C(x, 0) = x
+//			C(x, 0) = e^(-((x-5)/10)^2)
 //			C(0,t) = C(100,t)
 //
 //	Solution:
-//			C(x,t) = (x-v*t)*e^(-lambda*t)
+//			C(x,t) = e^(-((x - v*t - 30)/10)^2 - lambda*t)
 //
 //*****************************************************************************
 void moleProblem2(int myid){
 	int yCells = 1;
-	std::vector<int> numOfxCells{5};
-	std::vector<double> steps = {20};
+	std::vector<int> numOfxCells{1000};
+	std::vector<double> steps = {1};
 	double xLength = 100, yLength = 0.0; // cm
 	double tEnd = 20.0;	// seconds
-	double lambda = 0.1;	// 1/s
+	double lambda = 0.01;	// 1/s
 	double velocity = 2.0; // cm/s
 	double cSol, cCon;
 	int cID;
@@ -166,7 +172,7 @@ void moleProblem2(int myid){
 	std::ofstream outputFile;
 	outputFileName = "moleproblem2.out";
 	outputFile.open(outputFileName, std::ios::out | std::ios::trunc);
-	outputFile << "dx"	<< " " << "dt" << " " << "max l2 error" << "\n";
+	outputFile << "Total problem time: " << tEnd << "\n";
 
 	// loops over number of cells
 	for (int &xCells : numOfxCells){
@@ -180,13 +186,12 @@ void moleProblem2(int myid){
 		// loops over number of time steps
 		for (double &numofsteps	: steps){
 			dt = tEnd/numofsteps;
+			outputFile << "dx: " << xLength/(double)xCells << "\n";
+			outputFile << "dt: " << dt << "\n";
+			outputFile << "x"	<< " " << "C" << "\n";
 
 			// add specs
 			cID = spec.addSpecies(1.0, 0.0, 0.0);
-
-			// set periodic bcs
-			spec.setBoundaryCondition("periodic","east", cID);
-			spec.setBoundaryCondition("periodic","west", cID);
 
 			// sets the intial condition and sources
 			for (int i = 0; i < xCells; i++){
@@ -196,11 +201,11 @@ void moleProblem2(int myid){
 					// calculates the x positions as the cell faces
 					dx = cell->dx;
 					xc = cell->x;
-					x2 = xc + dx/2;
-					x1 = xc - dx/2;
+					x2 = xc + dx/2.;
+					x1 = xc - dx/2.;
 
 					// calculates the initial concentration from mvt. 
-					initCon = (1./(2.*dx))*(x2*x2 - x1*x1);		
+					initCon = (5.*(std::pow(M_PI, 0.5))/(dx))*(erf(3.-x1/10.) - erf(3.-x2/10.));
 
 					spec.setSpeciesCon(i, j, cID, initCon);
 
@@ -214,31 +219,30 @@ void moleProblem2(int myid){
 				t = step*dt;
 				// solve with cram
 				spec.solve(t);
-				//spec.solveimplicit(t);
+				//spec.solveImplicit(t);
+			}
+			// gets species concentrations
+			if (myid==0){
+				for (int i = 0; i < xCells; i++){
+					for (int j = 0; j < yCells; j++){
+						cell = model.getCellByLoc(i,j);	
 
-				std::cout << t << std::endl;
-				// gets species concentrations
-				if (myid==0){
-					for (int i = 0; i < xCells; i++){
-						for (int j = 0; j < yCells; j++){
-							cell = model.getCellByLoc(i,j);	
+						// caclulate analytical solution
+						xc = cell->x;
+						cSol = exp(-((xc-velocity*t-30.)/10.)*((xc-velocity*t-30.)/10.) - lambda*t);
 
-							// caclulate analytical solution
-							xc = cell->x;
-							cSol = (xc - velocity*t)*exp(-lambda*t);
+						// get libowski solution
+						cCon = spec.getSpecies(i, j, cID);
 
-							// get libowski solution
-							cCon = spec.getSpecies(i, j, cID);
-
-							//assert(isapprox(vsol, vcon, 1e-5, 1e-4));
-							linfError = std::max(linfError, std::abs(cSol-cCon)/cSol);
-							std::cout << cSol << " " << cCon << std::endl;
-						}
+						//assert(isapprox(vsol, vcon, 1e-5, 1e-4));
+						linfError = std::max(linfError, std::abs(cSol-cCon)/cSol);
+						outputFile << xc << " " << cSol << " " << cCon << "\n";
+						std::cout << cSol << " " << cCon << std::endl;
 					}
 				}
-				//outputfile << dx << " " << dt << " " << linferror << "\n";
-				std::cout << dx << " " << dt << " " << linfError << "\n";
 			}
+			outputFile << "\n";
+			//std::cout << dx << " " << dt << " " << linfError << "\n";
 			// clean species
 			spec.clean();
 		}
@@ -263,13 +267,13 @@ void moleProblem2(int myid){
 //			x = [0, 100]
 //			t = [0, 20]
 //			lambda = 1.0
-//			R(T) = A1*e^(A2*T)
+//			R(T) = A1*e^(A2/T)
 //			A1 = 1.0
-//			A2 = 1./3.
+//			A2 = -5000./8.31
 //			T = 900
 //
 //	Initial conditions and BC's:
-//			Ci(x, 0) = x
+//			Ci(x, 0) = x + 10
 //			Ci(0,t) = Ci(100,t)
 //			Cw(x, 0) = 0
 //			Cw(0,t) = Cw(100,t)
@@ -300,7 +304,7 @@ void moleProblem3(int myid){
 	outputFileName = "moleproblem3.out";
 	outputFile.open(outputFileName, std::ios::out | std::ios::trunc);
 	outputFile << "dx"	<< " " << "dt" << " " << "max l2 error" << "\n";
-	std::cout << "R " << R << std::endl;
+	outputFile << "Total problem time: " << tEnd << "\n";
 
 	// loops over number of cells
 	for (int &xCells : numOfxCells){
@@ -312,6 +316,9 @@ void moleProblem3(int myid){
 		// loops over number of time steps
 		for (double &numofsteps	: steps){
 			dt = tEnd/numofsteps;
+			outputFile << "dx: " << xLength/(double)xCells << "\n";
+			outputFile << "dt: " << dt << "\n";
+			outputFile << "x " << "Ci " << "Cw "<< "\n";
 
 			// add specs
 			CiID = spec.addSpecies(1.0, 0.0, 0.0);
@@ -329,7 +336,7 @@ void moleProblem3(int myid){
 					x1 = xc - dx/2;
 
 					// calculates the initial concentration from mvt. 
-					initCon = (1./(2.*dx))*(x2*x2 - x1*x1);		
+					initCon = (1./(dx))*(0.5*(x2*x2 - x1*x1) + 10.*(x2 - x1));
 
 					spec.setSpeciesCon(i, j, CiID, initCon);
 
@@ -344,32 +351,30 @@ void moleProblem3(int myid){
 				t = step*dt;
 				// solve with cram
 				spec.solve(t);
+			}
+			// gets species concentrations
+			if (myid==0){
+				for (int i = 0; i < xCells; i++){
+					for (int j = 0; j < yCells; j++){
+						cell = model.getCellByLoc(i,j);	
 
-				std::cout << t << std::endl;
-				// gets species concentrations
-				if (myid==0){
-					for (int i = 0; i < xCells; i++){
-						for (int j = 0; j < yCells; j++){
-							cell = model.getCellByLoc(i,j);	
+						// caclulate analytical solution
+						xc = cell->x;
+						CiSol = xc - R*t + 10.;
+						CwSol = R*t;
 
-							// caclulate analytical solution
-							xc = cell->x;
-							CiSol = xc - R*t;
-							CwSol = R*t;
+						// get libowski solution
+						CiCon = spec.getSpecies(i, j, CiID);
+						CwCon = spec.getSpecies(i, j, CwID);
 
-							// get libowski solution
-							CiCon = spec.getSpecies(i, j, CiID);
-							CwCon = spec.getSpecies(i, j, CwID);
-
-							//assert(isapprox(vsol, vcon, 1e-5, 1e-4));
-							//linfError = std::max(linfError, std::abs(csol-ccon)/csol);
-							std::cout << CiSol << " " << CiCon << " " << CwSol << " " << CwCon << std::endl;
-						}
+						//assert(isapprox(vsol, vcon, 1e-5, 1e-4));
+						//linfError = std::max(linfError, std::abs(csol-ccon)/csol);
+						outputFile << xc << " " << CiCon << " " << CwCon << "\n";
 					}
 				}
-				//outputfile << dx << " " << dt << " " << linferror << "\n";
-				std::cout << dx << " " << dt << " " << linfError << "\n";
 			}
+			outputFile << "\n";
+			//std::cout << dx << " " << dt << " " << linfError << "\n";
 			// clean species
 			spec.clean();
 		}
@@ -393,19 +398,20 @@ void moleProblem3(int myid){
 //			t = [0, 20]
 //			lambda = 1.0
 //			v = 2.0
-//			R(T) = A1*e^(A2*T)
+//			R(T) = A1*e^(A2/T)
 //			A1 = 1.0
-//			A2 = 1./3.
+//			A2 = -5000./8.31
 //			T = 900
 //
 //	Initial conditions and BC's:
-//			Ci(x, 0) = x
+//			Ci(x, 0) = e^(-((x-30)/10)^2) + 10
 //			Ci(0,t) = Ci(100,t)
 //			Cw(x, 0) = 0
 //			Cw(0,t) = Cw(100,t)
 //
-//	Solution: Need to fix this
+//	Solution: 
 //			Ci(x,t) = Ci0 - R(T)*t
+//			C(x,t) = e^(-((x - v*t - 30)/10)^2) - R*t + 10
 //			Cw(x,t) = R(T)*t
 //
 //*****************************************************************************
@@ -414,7 +420,7 @@ void moleProblem4(int myid){
 	std::vector<int> numOfxCells{10};
 	std::vector<double> steps = {1};
 	double xLength = 100, yLength = 0.0; // cm
-	double tEnd = 20.0;	// seconds
+	double tEnd = 10.0;	// seconds
 	double A1 = 1.0, A2 = 5000./8.31, T = 900.;
 	double velocity = 2.0; // cm/s
 	double R = A1*exp(-A2/T);
@@ -430,7 +436,7 @@ void moleProblem4(int myid){
 	std::ofstream outputFile;
 	outputFileName = "moleproblem4.out";
 	outputFile.open(outputFileName, std::ios::out | std::ios::trunc);
-	outputFile << "dx"	<< " " << "dt" << " " << "max l2 error" << "\n";
+	outputFile << "Total problem time: " << tEnd << "\n";
 
 	// loops over number of cells
 	for (int &xCells : numOfxCells){
@@ -444,6 +450,9 @@ void moleProblem4(int myid){
 		// loops over number of time steps
 		for (double &numofsteps	: steps){
 			dt = tEnd/numofsteps;
+			outputFile << "dx: " << xLength/(double)xCells << "\n";
+			outputFile << "dt: " << dt << "\n";
+			outputFile << "x " << "Ci " << "Cw "<< "\n";
 
 			// add specs
 			CiID = spec.addSpecies(1.0, 0.0, 0.0);
@@ -461,7 +470,8 @@ void moleProblem4(int myid){
 					x1 = xc - dx/2;
 
 					// calculates the initial concentration from mvt. 
-					initCon = (1./(2.*dx))*(x2*x2 - x1*x1);		
+					initCon = (5.*(std::pow(M_PI, 0.5))/(dx))*(erf(3.-x1/10.) - erf(3.-x2/10.));
+					initCon += (10.*x2 - 10.*x1);
 
 					spec.setSpeciesCon(i, j, CiID, initCon);
 
@@ -476,32 +486,31 @@ void moleProblem4(int myid){
 				t = step*dt;
 				// solve with cram
 				spec.solve(t);
+			}
+			// gets species concentrations
+			if (myid==0){
+				for (int i = 0; i < xCells; i++){
+					for (int j = 0; j < yCells; j++){
+						cell = model.getCellByLoc(i,j);	
 
-				std::cout << t << std::endl;
-				// gets species concentrations
-				if (myid==0){
-					for (int i = 0; i < xCells; i++){
-						for (int j = 0; j < yCells; j++){
-							cell = model.getCellByLoc(i,j);	
+						// caclulate analytical solution
+						xc = cell->x;
+						CiSol = exp(-((xc-velocity*t-30.)/10.)*((xc-velocity*t-30.)/10.)) -
+							R*t + 10.;
+						CwSol = R*t;
 
-							// caclulate analytical solution
-							xc = cell->x;
-							CiSol = xc - R*t;
-							CwSol = R*t;
+						// get libowski solution
+						CiCon = spec.getSpecies(i, j, CiID);
+						CwCon = spec.getSpecies(i, j, CwID);
 
-							// get libowski solution
-							CiCon = spec.getSpecies(i, j, CiID);
-							CwCon = spec.getSpecies(i, j, CwID);
-
-							//assert(isapprox(vsol, vcon, 1e-5, 1e-4));
-							//linfError = std::max(linfError, std::abs(csol-ccon)/csol);
-							std::cout << CiSol << " " << CiCon << " " << CwSol << " " << CwCon << std::endl;
-						}
+						//assert(isapprox(vsol, vcon, 1e-5, 1e-4));
+						//linfError = std::max(linfError, std::abs(csol-ccon)/csol);
+						outputFile << xc << " " << CiCon << " " << CwCon << "\n";
 					}
 				}
-				//outputfile << dx << " " << dt << " " << linferror << "\n";
-				std::cout << dx << " " << dt << " " << linfError << "\n";
 			}
+			outputFile << "\n";
+			//std::cout << dx << " " << dt << " " << linfError << "\n";
 			// clean species
 			spec.clean();
 		}
@@ -510,14 +519,150 @@ void moleProblem4(int myid){
 	}
 }
 
+//*****************************************************************************
+// Mole problem 5
+//
+// This problem is the same as problem 4 but with temperature that varies in 
+// the x direction. Source term is constant but varies in the x direction,
+// so need to use MVT and integrate over the cell. 
+//
+//	Problem equations:
+//			dCi/dt = -v*dCi/dx-R(T)
+//			dCw/dt = R(T)
+//
+//	Domaine:
+//			x = [0, 100]
+//			t = [0, 20]
+//			lambda = 1.0
+//			v = 2.0
+//			R(T) = A1*e^(A2/T)
+//			T = 850 + 10
+//			A1 = 1.0
+//			A2 = -5000./8.31
+//			T = 900
+//
+//	Initial conditions and BC's:
+//			Ci(x, 0) = e^(-((x-30)/10)^2) + 10
+//			Ci(0,t) = Ci(100,t)
+//			Cw(x, 0) = 0
+//			Cw(0,t) = Cw(100,t)
+//
+//	Solution: Need to fix this
+//			C(x,t) = e^(-((x - v*t - 30)/10)^2) - R*t + 10
+//			Cw(x,t) = R(T)*t
+//
+//*****************************************************************************
+void moleProblem5(int myid){
+	int yCells = 1;
+	std::vector<int> numOfxCells{10};
+	std::vector<double> steps = {1};
+	double xLength = 100, yLength = 0.0; // cm
+	double tEnd = 10.0;	// seconds
+	double A1 = 1.0, A2 = 5000./8.31, T = 900.;
+	double velocity = 2.0; // cm/s
+	double R = A1*exp(-A2/T);
+	double CiSol, CwSol, CiCon, CwCon;
+	int CiID, CwID;
+	double x1, x2, initCon, dx, xc, dt, t;
+	double linfError = 0.0;
+	meshCell* cell = nullptr;
+	std::string outputFileName;
+	std::vector<double> Cicoeffs = {0.0, 0.0};
+	std::vector<double> Cwcoeffs = {0.0, 0.0};
+	// sets the ouput file name
+	std::ofstream outputFile;
+	outputFileName = "moleproblem5.out";
+	outputFile.open(outputFileName, std::ios::out | std::ios::trunc);
+	outputFile << "Total problem time: " << tEnd << "\n";
+
+	// loops over number of cells
+	for (int &xCells : numOfxCells){
+		// build the mesh
+		modelMesh model(xCells, yCells, xLength, yLength);
+		// build species driver
+		speciesDriver spec = speciesDriver(&model);
+		// set x velocity
+		model.setConstantXVelocity(velocity);
+
+		// loops over number of time steps
+		for (double &numofsteps	: steps){
+			dt = tEnd/numofsteps;
+			outputFile << "dx: " << xLength/(double)xCells << "\n";
+			outputFile << "dt: " << dt << "\n";
+			outputFile << "x " << "Ci " << "Cw "<< "\n";
+
+			// add specs
+			CiID = spec.addSpecies(1.0, 0.0, 0.0);
+			CwID = spec.addSpecies(1.0, 0.0, 0.0);
+
+			// sets the intial condition and sources
+			for (int i = 0; i < xCells; i++){
+				for (int j = 0; j < yCells; j++){
+					cell = model.getCellByLoc(i,j);	
+
+					// calculates the x positions as the cell faces
+					dx = cell->dx;
+					xc = cell->x;
+					x2 = xc + dx/2;
+					x1 = xc - dx/2;
+
+					// calculates the initial concentration from mvt. 
+					initCon = (5.*(std::pow(M_PI, 0.5))/(dx))*(erf(3.-x1/10.) - erf(3.-x2/10.));
+					initCon += (10.*x2 - 10.*x1);
+
+					spec.setSpeciesCon(i, j, CiID, initCon);
+
+					// sets the sources
+					spec.setSpeciesSource(i, j, CiID, Cicoeffs, -R);
+					spec.setSpeciesSource(i, j, CwID, Cwcoeffs, R);
+				}
+			}
+			t = 0.0;
+			// solve the problem
+			for (int step = 1; step <= numofsteps; step++){
+				t = step*dt;
+				// solve with cram
+				spec.solve(t);
+			}
+			// gets species concentrations
+			if (myid==0){
+				for (int i = 0; i < xCells; i++){
+					for (int j = 0; j < yCells; j++){
+						cell = model.getCellByLoc(i,j);	
+
+						// caclulate analytical solution
+						xc = cell->x;
+						CiSol = xc - R*t;
+						CwSol = R*t;
+
+						// get libowski solution
+						CiCon = spec.getSpecies(i, j, CiID);
+						CwCon = spec.getSpecies(i, j, CwID);
+
+						//assert(isapprox(vsol, vcon, 1e-5, 1e-4));
+						//linfError = std::max(linfError, std::abs(csol-ccon)/csol);
+						outputFile << xc << " " << CiCon << " " << CwCon << "\n";
+					}
+				}
+			}
+			outputFile << "\n";
+			//std::cout << dx << " " << dt << " " << linfError << "\n";
+			// clean species
+			spec.clean();
+		}
+		spec.clean();
+		model.clean();
+	}
+}
 int main(){
 	int myid = mpi.rank;
 	int numprocs = mpi.size;
 
-	//moleProblem1(myid);
-	//moleProblem2(myid);
-	//moleProblem3(myid);
+	moleProblem1(myid);
+	moleProblem2(myid);
+	moleProblem3(myid);
 	moleProblem4(myid);
+	moleProblem5(myid);
 
-
+	mpi.finalize();
 }
