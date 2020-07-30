858b488bcdfa1766df6e86d5c1fe546f8e932c42
Zack Taylor <rtaylo45@vols.utk.edu>
2020-07-30 14:40:05 -0400

    Got mole problem 3 solution working now

--- caseStudies/caseStudies.cpp
+++ caseStudies/caseStudies.cpp
@@ -4,6 +4,30 @@
 #include "meshCellData.h"
 #include "species.h"
 
+//**************************************************************************
+// A "Lump" depletion case with a single cell. 
+//
+//	Problem equations:
+//		dCi/dt = sum^{J}_{j=1} A_{i,j}*C_{j}
+//
+//	Domaine:
+//			x = [0, 220]	cm
+//			y = [0, 220]	cm
+//			t = [0, 10]		y
+//
+//	Initial conditions and BC's:
+//			C_{H-1}		= 2.74768E28
+//			C_{B-10}		= 1.001E25
+//			C_{O-16}		= 2.7503E28
+//			C_{Zr-91}	= 4.14467E26
+//			C_{Zr-96}	= 1.03432E26
+//			C_{U-235}	= 1.909E26
+//			C_{U-238}	= 6.592E27
+//
+//	Solution:
+//		Calculated by matlab using year long time steps
+// 
+//**************************************************************************
 void singleCellDepletion(int myid, std::string solverType){
 	double t = 0.0;
 	int steps = 10;
@@ -11,11 +35,11 @@ void singleCellDepletion(int myid, std::string solverType){
 	double totalTime = depletionTime*24.*60.*60.;
 	double dt = totalTime/steps;
 	int xCells = 1, yCells = 1;
-	double xLength = 1.0, yLength = 1.0;
+	double xLength = 220.0/1000., yLength = 220.0/1000.;
 	std::vector<int> ids;
 	std::string path = getDataPath();
-	std::string outputFileName = "caseStudyOne.out";
-	std::string outputFileNameMatlab = "caseStudyOne"+solverType+".csv";
+	std::string outputFileName = "caseStudy1.out";
+	std::string outputFileNameMatlab = "caseStudy1"+solverType+".csv";
 	std::ofstream outputFile, outputFileMatlab;
 	outputFile.open(outputFileName, std::ios_base::app);
 	outputFileMatlab.open(outputFileNameMatlab);
@@ -28,6 +52,7 @@ void singleCellDepletion(int myid, std::string solverType){
 
 	// Builds the model
 	modelMesh model(xCells, yCells, xLength, yLength);
+
 	// Builds the species object
 	speciesDriver spec = speciesDriver(&model);
 
@@ -45,9 +70,12 @@ void singleCellDepletion(int myid, std::string solverType){
 	outputFile << "solverName: " << solverType << std::endl;
 	outputFile.precision(16); 
 
-	spec.writeTransitionMatrixToFile("transitionMatrixCaseStudyOne.csv");
-
+	// Writes transition matrix and initial condition
+	spec.writeTransitionMatrixToFile("transitionMatrixCaseStudy1.csv");
+	spec.writeInitialConditionToFile("initialConditionCaseStudy1.csv");
 	MatrixD solData = MatrixD::Zero(ids.size()+1,10);
+
+	// Loops over the time steps to solve
 	for (int k = 0; k < steps; k++){
 		t = t + dt;
 		spec.solve(t);
